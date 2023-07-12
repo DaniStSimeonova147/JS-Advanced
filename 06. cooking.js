b0c050d063f42558f2a596b73db1b28d2df6d4f0
DaniStSimeonova147 <48598905+DaniStSimeonova147@users.noreply.github.com>
2023-07-12 16:37:37 +0300

    Add files via upload
    Exercise: Syntax, Functions and Statements

---
+++ 06. cooking.js
@@ -0,0 +1,30 @@
+function solve([num, ...params]) {
+    num = Number(num);
+
+    for (let operation of params) {
+        switch (operation) {
+            case 'chop':
+                num /= 2;
+                break;
+            case 'dice':
+                num = Math.sqrt(num);
+                break;
+            case 'spice':
+                num += 1;
+                break;
+            case 'bake':
+                num *= 3;
+                break;
+            case 'fillet':
+                num *= 0.8;
+                break;
+
+            default:
+                break;
+        }
+        console.log(num);
+    }
+}
+
+solve(['32', 'chop', 'chop', 'chop', 'chop', 'chop']);
+solve(['9', 'dice', 'spice', 'chop', 'bake', 'fillet']);
